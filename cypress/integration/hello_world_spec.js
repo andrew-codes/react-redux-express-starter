4e738e48a00ebab00b639ac523569f0315aae4a3
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2017-10-21 03:16:33 -0400

    Scripts to exercies the login page.

--- cypress/integration/hello_world_spec.js
+++ cypress/integration/hello_world_spec.js
@@ -1,7 +1,31 @@
-describe('Hello world', () => {
-    it('Login page loads', () => {
+describe('Given a valid username password attempt', () => {
+    it('shows no error messages', () => {
         cy.visit('http://localhost:3004/');
 
-        cy.contains('password');
+        cy.get('#email-address')
+            .type('fake@email.com', {force: true})
+            .should('have.value', 'fake@email.com')
+
+
+        cy.get('#password')
+            .type('password', {force: true})
+            .should('have.value', 'password')
+
+        cy.get('.error_container')
+            .should('have.class', 'invisible')
+    })
+});
+
+describe('Given the password is too short', () => {
+    it('shows correct error message', () => {
+        cy.visit('http://localhost:3004/');
+
+        cy.get('#password')
+            .type('a', {force: true})
+            .should('have.value', 'a')
+
+        cy.get('.error_container')
+            .contains('Try a stronger password.')
+            .should('not.have.class', 'invisible')
     })
 });
